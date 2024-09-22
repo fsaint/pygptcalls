e33c04bfd6a53f213d9ae9e7a1bfb09fba6a9d21
Felipe Saint-Jean <fsaint@helloseer.com>
2024-09-21 22:53:32 -0700

    Arrggh

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="@fsaint",  # Your name or the author's name
     author_email="fsaint@gmail.com",  # Your email address
     url="https://github.com/fsaint/pygptcalls",  # URL to your GitHub repo (optional)
-    packages=find_packages(),  # Automatically find all packages in this directory
+    packages=['pygptcalls'],
     install_requires=['annotated-types==0.7.0', 'anyio==4.4.0', 'certifi==2024.8.30', 'distro==1.9.0', 'h11==0.14.0', 'httpcore==1.0.5', 'httpx==0.27.2', 'idna==3.10', 'jiter==0.5.0', 'openai==1.46.0', 'pydantic==2.9.2', 'pydantic_core==2.23.4', 'sniffio==1.3.1', 'tqdm==4.66.5', 'typing_extensions==4.12.2'],
     classifiers=[
         'Programming Language :: Python :: 3',
