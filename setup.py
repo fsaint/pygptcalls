53538f6922831c8f944e8e00a5ab37bdd11ded0b
Felipe Saint-Jean <fsaint@helloseer.com>
2024-12-22 20:47:09 -0800

    Updated main

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="@fsaint",  # Your name or the author's name
     author_email="fsaint@gmail.com",  # Your email address
     url="https://github.com/fsaint/pygptcalls",  # URL to your GitHub repo (optional)
-    packages=['pygptcalls','pygptcalls.tools'],
+    packages=find_packages(),
     install_requires=['openai>1.46.0,<2.0.0', 'pydantic==2.9.2'],
     classifiers=[
         'Programming Language :: Python :: 3',
